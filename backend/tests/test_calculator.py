d17df638a46950dbce7c8cb703a37ff709d251d1
Jiean Yang <jiean.yang.freelancer@gmail.com>
2024-04-18 17:33:40 +0200

    Add more test

--- backend/tests/test_calculator.py
+++ backend/tests/test_calculator.py
@@ -6,15 +6,31 @@ class TestCalculator(TestCase):
     def setUp(self):
         self.calculator = Calculator()
 
-    def test_get_expression_result_success(self):
+    def test_get_expression_result_success_1_integer(self):
         self.assertEqual(self.calculator.get_expression_result("5 1 2 + 4 * + 3 -"), 14)
         self.assertEqual(self.calculator.get_expression_result("1 2 +"), 3)
         self.assertEqual(self.calculator.get_expression_result("1 2 + 3 *"), 9)
+    
+    def test_get_expression_result_success_2_decimal_number(self):
         self.assertEqual(self.calculator.get_expression_result("1 2 + 3 * 4 /"), 2.25)
         self.assertEqual(
             self.calculator.get_expression_result("1 2 + 3 * 4 / 5 +"), 7.25
         )
+    
+    def test_get_expression_result_success_3_large_space_distance_between_number(self):
+        self.assertEqual(self.calculator.get_expression_result("5     1  2 +    4 * + 3  -"), 14)
+        self.assertEqual(self.calculator.get_expression_result("1 2     +"), 3)
+        self.assertEqual(self.calculator.get_expression_result("1     2 + 3 *"), 9)
+
+    def test_get_express_result_success_3_large_number(self):
+        self.assertEqual(
+            self.calculator.get_expression_result("1    2 +      100 *"), 300
+        )
         
+    def test_get_express_result_success_4_with_negative_nubmer(self):
+        self.assertEqual(
+            self.calculator.get_expression_result("1    2 +      -1 *"), -3
+        )
 
     def test_get_expression_result_error_case_1_not_enough_number_for_operation(self):
         try:
